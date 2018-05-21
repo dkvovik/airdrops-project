801600229bd69c2c9f05e253f394e56823326243
dkvovik <vs.berdnikov@mail.ru>
2018-05-21 13:15:52 +0300

    Updated Filter Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
+import { MatTabsModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RangeSliderModule } from 'ngx-rangeslider-component';
 
+import { AppRoutingModule } from './app.routing';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-import { MatSliderModule, MatTabsModule } from '@angular/material';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomePageComponent } from './home-page/home-page.component';
-import { AppRoutingModule } from './app.routing';
-import { RouterModule } from '@angular/router';
 import { FilterComponent } from './filter/filter.component';
 import { AirdropService } from './services/airdrop.service';
-import { HttpClientModule } from '@angular/common/http';
 import { FindAirdropComponent } from './find-airdrop/find-airdrop.component';
+
 
 
 @NgModule({
@@ -28,11 +31,12 @@ import { FindAirdropComponent } from './find-airdrop/find-airdrop.component';
   imports: [
     BrowserModule,
     RouterModule,
+    FormsModule,
     AppRoutingModule,
     HttpClientModule,
     MatTabsModule,
     BrowserAnimationsModule,
-    MatSliderModule
+    RangeSliderModule
   ],
   providers: [AirdropService],
   bootstrap: [AppComponent]
--- src/app/filter/filter.component.ts
+++ src/app/filter/filter.component.ts
@@ -10,6 +10,18 @@ import { Airdrop } from '../shared/models/airdrop';
 export class FilterComponent implements OnInit {
 
   airdrops: Airdrop[] = [];
+
+  minTokenValue = 0;
+  maxTokenValue = 100;
+  stepTokenValue = 1;
+  twoWayRangeTokenValue = [10, 30];
+
+  minRating = 0;
+  maxRating = 100;
+  stepRating = 1;
+  twoWayRangeRating = [0, 100];
+
+
   constructor(private airdropService: AirdropService) { }
 
   ngOnInit() {
@@ -18,6 +30,20 @@ export class FilterComponent implements OnInit {
       console.log('this.airdrops', this.airdrops);
     }, 5000);
   }
+
+
+  changed() {
+    this.twoWayRangeTokenValue = [...this.twoWayRangeTokenValue];
+  }
+
+  changedRating() {
+    this.twoWayRangeRating = [...this.twoWayRangeRating];
+  }
+
+  clearFormFilter() {
+    /******/
+  }
+
 
   getAirdrops() {
     this.airdropService.getAirdrops().subscribe(
