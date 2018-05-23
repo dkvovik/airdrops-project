9065ec9bd36ed35a55a7b8e2a78d83dd9c79152f
dkvovik <vs.berdnikov@mail.ru>
2018-05-23 17:36:04 +0300

    Added filtering by status.

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, TemplateRef } from '@angular/core';
 import { BsModalRef, BsModalService } from 'ngx-bootstrap';
+import { AirdropService } from '../services/airdrop.service';
+import { Airdrop } from '../shared/models/airdrop';
 
 @Component({
   selector: 'app-home-page',
@@ -10,13 +12,46 @@ export class HomePageComponent implements OnInit {
 
   modalRef: BsModalRef;
 
-  constructor(private modalService: BsModalService) { }
+  airdrops: Airdrop[] = [];
+  upcominngAirdrop: Airdrop[] = [];
+  activeAirdrop: Airdrop[] = [];
+  pastAirdrop: Airdrop[] = [];
+
+  date = new Date();
+  today = this.date.getDate();
+  yesterday = this.date.getDate() - 1;
+
+  constructor(private modalService: BsModalService,
+              private airdropService: AirdropService) {
+  }
 
   ngOnInit() {
+    this.getAirdrops();
   }
 
   openModal(template: TemplateRef<any>) {
     this.modalRef = this.modalService.show(template);
+  }
+
+  getAirdrops() {
+    this.airdropService.getAirdrops().subscribe(
+      (d: any) => {
+        this.airdrops = d;
+        this.isTodayOrYesterday(this.airdrops);
+      },
+      (error)  => console.log('Error getAirdrops' , error)
+      );
+  }
+
+  isTodayOrYesterday(airdrops) {
+    airdrops.forEach( (a) => {
+      const startDate = new Date(a.startDate).getDate();
+      if (startDate === this.today) {
+        a['today'] = true;
+      } else if (startDate === this.yesterday) {
+        a['yesterday'] = true;
+      }
+    });
   }
 
 }
