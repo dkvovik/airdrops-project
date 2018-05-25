e6207f6f864d7a1332b023a0c5ea4b7bcc9bae9a
dkvovik <vs.berdnikov@mail.ru>
2018-05-25 18:00:42 +0300

    Added method "parseIconProjectLink".

--- src/app/finded-airdrop/finded-airdrop.component.ts
+++ src/app/finded-airdrop/finded-airdrop.component.ts
@@ -24,24 +24,36 @@ export class FindedAirdropComponent implements OnInit {
 
   openModalWithComponent(airdrop) {
     const initialState = {...airdrop};
-    initialState['requirementsIcon'] = ['Twitter', 'Github', 'Google-plus'];
     this.modalRef = this.modalService.show(DetailAirdropComponent, {initialState});
   }
 
-  parseIconLink(link) {
-    link = link.split(/([a-zA-Z0-9-_]+\.\w+)[\/]/)[0];
-    console.log('link', link);
-
+  parseIconProjectLink(link) {
     let iconLink = '';
 
+    link = link.split(/([a-zA-Z0-9-_]+\.\w+)[\/]/)[1];
+
     switch (link) {
-      case 'twitter':
+      case 'twitter.com':
         iconLink =  `<svg viewBox="0 0 20.162 16.507" class="icon soc-net">
                   <g id="twitter" transform="translate(0 -0.94)">
                     <path id="XMLID_827_" class="cls-1" d="M19.7,30a7.643,7.643,0,0,1-.975.355,4.311,4.311,0,0,0,.878-1.543.325.325,0,0,0-.476-.379,7.674,7.674,0,0,1-2.268.9A4.33,4.33,0,0,0,9.5,32.425a4.415,4.415,0,0,0,.035.559,11.081,11.081,0,0,1-7.606-4.035.325.325,0,0,0-.533.042,4.334,4.334,0,0,0,.444,4.982,3.668,3.668,0,0,1-.579-.259.325.325,0,0,0-.483.277c0,.019,0,.038,0,.058A4.344,4.344,0,0,0,2.9,37.771c-.11-.011-.22-.027-.329-.048a.325.325,0,0,0-.371.419,4.327,4.327,0,0,0,3.171,2.9,7.657,7.657,0,0,1-4.093,1.168,7.835,7.835,0,0,1-.917-.054.325.325,0,0,0-.214.6A11.672,11.672,0,0,0,6.459,44.6a11.229,11.229,0,0,0,8.7-3.821,12.112,12.112,0,0,0,3.015-7.894c0-.119,0-.239-.005-.358a8.414,8.414,0,0,0,1.936-2.051A.325.325,0,0,0,19.7,30Z" transform="translate(0.002 -27.157)"/>
                   </g>
                 </svg>`;
         break;
+      case 'facebook.com':
+        iconLink = `<svg class="icon soc-net" viewBox="0 0 16.408 16.408">
+          <g id="facebook-logo" class="cls-1">
+            <path id="Facebook__x28_alt_x29_" class="cls-2" d="M16.408,2.735A2.834,2.834,0,0,0,13.673,0H2.735A2.834,2.834,0,0,0,0,2.735V13.673a2.834,2.834,0,0,0,2.735,2.735H8.2v-6.2H6.2V7.475H8.2V6.409a3.365,3.365,0,0,1,3.077-3.492h2.211V5.652H11.281c-.242,0-.524.294-.524.734V7.475h2.735v2.735H10.756v6.2h2.917a2.834,2.834,0,0,0,2.735-2.735Z" transform="translate(0 0)"/>
+          </g>
+        </svg>`;
+        break;
+      case 'google.com':
+        iconLink = `<svg id="google-plus" class="icon soc-net" viewBox="0 0 24.294 15.544">
+          <path id="Path_966" data-name="Path 966" class="cls-1" d="M8.524,91.895h3.8a4.825,4.825,0,1,1-1.31-5.174.523.523,0,0,0,.711,0l1.394-1.312a.521.521,0,0,0,0-.758,7.743,7.743,0,0,0-5.189-2.128,7.772,7.772,0,1,0,7.6,8.208c.006-.051.01-1.784.01-1.784H8.524A.521.521,0,0,0,8,89.466v1.908A.521.521,0,0,0,8.524,91.895Z" transform="translate(0 -82.519)"/>
+          <path id="Path_967" data-name="Path 967" class="cls-1" d="M328.663,165.529v-1.865a.456.456,0,0,0-.456-.456h-1.569a.456.456,0,0,0-.457.456v1.865h-1.865a.456.456,0,0,0-.456.456v1.569a.456.456,0,0,0,.456.456h1.865v1.865a.456.456,0,0,0,.457.456h1.569a.456.456,0,0,0,.456-.456v-1.865h1.865a.456.456,0,0,0,.456-.456v-1.569a.456.456,0,0,0-.456-.456Z" transform="translate(-306.691 -158.93)"/>
+        </svg>`;
+        break;
+
     }
 
     return this.sanitizer.bypassSecurityTrustHtml(iconLink);
@@ -92,14 +104,14 @@ export class FindedAirdropComponent implements OnInit {
     </div>
     <div class="modal-subheader-detail">
       <div class="subheader-detail-item">
-        <span class="badge_grey">Asset ID</span>{{assetId}}
+        <div class="subheader-detail-asset"><span class="badge_grey">Asset ID</span>{{assetId}}</div>
         <span class="badge_grey">Platform</span>{{platform}}
       </div>
       <div class="subheader-detail-item">
         <div class="requirements-wrapper" >
           <span class="badge_grey">Requirements</span>
-          <span [ngSwitch]="requirement | lowercase" *ngFor="let requirement of requirementsIcon"
-                [popover]="requirement" triggers="mouseenter:mouseleave" class="requirement-item">
+          <span [ngSwitch]="requirement | lowercase" *ngFor="let requirement of requirements"
+                [popover]="requirementFunc(requirement)" triggers="mouseenter:mouseleave" class="requirement-item">
             <svg *ngSwitchCase="'email'" class="icon active disabled" viewBox="0 0 20.147 14.576">
               <g id="message-envelope" class="cls-1" transform="translate(0 -1)">
                 <g id="Group_129" data-name="Group 129" transform="translate(0 1)">
@@ -184,7 +196,13 @@ export class FindedAirdropComponent implements OnInit {
         <div class="project-links-wrapper">
           <div class="project-links-header">
             <div class="title">PROJECT LINKS</div>
-            <div class="project-links"></div>
+            <div class="project-links">
+              <div class="info-social-icon">
+                <a href="{{link}}" *ngFor="let link of projectLinks"
+                   [innerHtml]="parseIconProjectLink(link)">
+                </a>
+              </div>
+            </div>
           </div>
           <div class="project-links-footer">{{commentBlock}}</div>
         </div>
@@ -245,6 +263,11 @@ export class FindedAirdropComponent implements OnInit {
 
     .subheader-detail-item:first-child {
       margin-bottom: 10px;
+      display: flex;
+    }
+
+    .subheader-detail-asset {
+      margin-right: 20px;
     }
 
     .modal-body__header,
@@ -352,6 +375,7 @@ export class FindedAirdropComponent implements OnInit {
     .modal-body__middle .htg-content {
       display: flex;
       align-items: center;
+      margin-bottom: 20px;
     }
 
     .htg-index {
@@ -371,18 +395,25 @@ export class FindedAirdropComponent implements OnInit {
     }
 
     .project-links-wrapper {
-      background-color: #E2E2E2;
+      background-color: #EAEAEA;
     }
     .project-links-header {
-      border-bottom: 1px solid #EDEDED;
+      border-bottom: 1px solid #E2E2E2;
       padding-top: 23px;
       padding-bottom: 18px;
       display: flex;
-      justify-content: center;
+      flex-direction: column;
+      align-items: center;
+    }
+    .project-links-footer,
+    .project-links-footer {
+      padding-left: 15px;
+      padding-bottom: 15px;
     }
     .project-links-header .title {
       color: #4E5C6E;
       font-size: 0.8824rem;
+      margin-bottom: 15px;
     }
 
     .modal-footer {
@@ -409,10 +440,10 @@ export class DetailAirdropComponent implements OnInit {
 
   tokenName: string;
 
-  requirementsIcon: string[];
 
   constructor(public modalRef: BsModalRef,
-              private globals: Globals) {
+              private globals: Globals,
+              private sanitize: DomSanitizer) {
   }
 
   ngOnInit() {
@@ -420,6 +451,45 @@ export class DetailAirdropComponent implements OnInit {
       this.globals.visitedAirdrop.push(this.tokenName);
     }
     localStorage.setItem('visitedAirdrop', JSON.stringify(this.globals.visitedAirdrop));
+  }
+
+  requirementFunc(value) {
+    if (value === 'Twitter') {
+      return value + ' (Follow, Retweet, Tweet)';
+    } else {
+      return value;
+    }
+  }
+
+  parseIconProjectLink(link) {
+    let iconLink = '';
+
+    link = link.split(/([a-zA-Z0-9-_]+\.\w+)[\/]/)[1];
+
+    switch (link) {
+      case 'twitter.com':
+        iconLink =  `<svg viewBox="0 0 20.162 16.507" class="icon large active">
+                    <g id="twitter" transform="translate(0 -0.94)">
+                      <path id="XMLID_827_" class="cls-1" d="M19.7,30a7.643,7.643,0,0,1-.975.355,4.311,4.311,0,0,0,.878-1.543.325.325,0,0,0-.476-.379,7.674,7.674,0,0,1-2.268.9A4.33,4.33,0,0,0,9.5,32.425a4.415,4.415,0,0,0,.035.559,11.081,11.081,0,0,1-7.606-4.035.325.325,0,0,0-.533.042,4.334,4.334,0,0,0,.444,4.982,3.668,3.668,0,0,1-.579-.259.325.325,0,0,0-.483.277c0,.019,0,.038,0,.058A4.344,4.344,0,0,0,2.9,37.771c-.11-.011-.22-.027-.329-.048a.325.325,0,0,0-.371.419,4.327,4.327,0,0,0,3.171,2.9,7.657,7.657,0,0,1-4.093,1.168,7.835,7.835,0,0,1-.917-.054.325.325,0,0,0-.214.6A11.672,11.672,0,0,0,6.459,44.6a11.229,11.229,0,0,0,8.7-3.821,12.112,12.112,0,0,0,3.015-7.894c0-.119,0-.239-.005-.358a8.414,8.414,0,0,0,1.936-2.051A.325.325,0,0,0,19.7,30Z" transform="translate(0.002 -27.157)"/>
+                    </g>
+                  </svg>`;
+        break;
+      case 'facebook.com':
+        iconLink = `<svg class="icon large active" viewBox="0 0 16.408 16.408">
+          <g id="facebook-logo" class="cls-1">
+            <path id="Facebook__x28_alt_x29_" class="cls-2" d="M16.408,2.735A2.834,2.834,0,0,0,13.673,0H2.735A2.834,2.834,0,0,0,0,2.735V13.673a2.834,2.834,0,0,0,2.735,2.735H8.2v-6.2H6.2V7.475H8.2V6.409a3.365,3.365,0,0,1,3.077-3.492h2.211V5.652H11.281c-.242,0-.524.294-.524.734V7.475h2.735v2.735H10.756v6.2h2.917a2.834,2.834,0,0,0,2.735-2.735Z" transform="translate(0 0)"/>
+          </g>
+        </svg>`;
+        break;
+      case 'google.com':
+        iconLink = `<svg id="google-plus" class="icon large active" viewBox="0 0 24.294 15.544">
+          <path id="Path_966" data-name="Path 966" class="cls-1" d="M8.524,91.895h3.8a4.825,4.825,0,1,1-1.31-5.174.523.523,0,0,0,.711,0l1.394-1.312a.521.521,0,0,0,0-.758,7.743,7.743,0,0,0-5.189-2.128,7.772,7.772,0,1,0,7.6,8.208c.006-.051.01-1.784.01-1.784H8.524A.521.521,0,0,0,8,89.466v1.908A.521.521,0,0,0,8.524,91.895Z" transform="translate(0 -82.519)"/>
+          <path id="Path_967" data-name="Path 967" class="cls-1" d="M328.663,165.529v-1.865a.456.456,0,0,0-.456-.456h-1.569a.456.456,0,0,0-.457.456v1.865h-1.865a.456.456,0,0,0-.456.456v1.569a.456.456,0,0,0,.456.456h1.865v1.865a.456.456,0,0,0,.457.456h1.569a.456.456,0,0,0,.456-.456v-1.865h1.865a.456.456,0,0,0,.456-.456v-1.569a.456.456,0,0,0-.456-.456Z" transform="translate(-306.691 -158.93)"/>
+        </svg>`;
+        break;
+    }
+
+    return this.sanitize.bypassSecurityTrustHtml(iconLink);
   }
 
 }
